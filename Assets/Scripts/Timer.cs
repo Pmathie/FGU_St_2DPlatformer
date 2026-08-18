94163af0c0fe68c55714da3bdd314ef75e61ebec
Pmathie <cykelrytteren@hotmail.com>
2026-08-18 13:22:17 +0200

    Text and timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public TextMeshProUGUI timerText;
+    private float time = 0;
+    private bool pauseTime = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!pauseTime)
+        {
+            time = time + Time.deltaTime;
+        }
+
+        int minutes = Mathf.FloorToInt(time / 60);
+        int seconds = Mathf.FloorToInt(time % 60);
+        int milliseconds = Mathf.FloorToInt((time * 1000) % 1000);
+
+        timerText.text = $"{minutes:00} : {seconds:00} : {milliseconds:000}";
+    
+    }
+    public void StartTime()
+    {
+        pauseTime = false;
+    }
+    public void PauseTime()
+    {
+        pauseTime = true;
+    }
+    public void ResetTime()
+    {
+        time = 0;
+    }
+
+}
